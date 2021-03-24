7a5cb5656f7488b43d17a1b066e473ec678e7bc6
Amit Krishna A <54491362+akri16@users.noreply.github.com>
2021-03-24 16:19:09 +0530

    Add files via upload

---
+++ .github/check_CD.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+HOUR_IN_UNIX=3600
+
+set_value_at_key () {
+    curl -s --request PUT 'https://api.kvstore.io/collections/kv/items/$1' 
+            --header 'kvstoreio_api_key: ${KV_STORE_API_KEY}' 
+            --header 'Content-Type: text/plain' 
+            --data-raw '$2'
+}
+
+get_current_time () {
+    local current_time=$(curl -s https://api.keyvalue.xyz/timestamp)
+    echo ${current_time}
+}
+
+get_value_at_key () {
+    local value=$(curl -s --request GET 'https://api.kvstore.io/collections/kv/items/$1' 
+            --header 'kvstoreio_api_key: ${KV_STORE_API_KEY}' | jq -r '.value')
+    
+    echo ${value}
+}
+
+check_is_last_exec_time () {
+    local current_time=$(get_current_time)
+    local diff_unix=$((HOUR_IN_UNIX * get_value_at_key diff))
+    local last_exec=$(get_value_at_key last_exec)
+
+    echo $(( (current_time - last_exec) -gt diff_unix ))
+}
+
+
+if ([ $GITHUB_EVENT = "schedule" ] && [ $(get_value_at_key has_changes) = 1 ]) || [ $GITHUB_EVENT = "workflow_dispatch" ];
+then
+    set_value_at_key has_changes 0;
+    echo "::set-output name=should_run::true"
+
+else if [ $(check_is_last_exec_time) = 0 ];   
+then
+    set_value_at_key has_changes 1;
+else
+    set_value_at_key has_changes 0;
+    set_value_at_key last_exec $(get_current_time);
+fi
